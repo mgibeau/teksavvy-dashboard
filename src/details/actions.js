16f7a539b4509b4d5630f4d7eb8e35cea0d0e571
Greg Hamel <hamegreg@gmail.com>
2018-01-03 14:57:40 -0500

    This closes #4

--- src/details/actions.js
+++ src/details/actions.js
@@ -20,15 +20,16 @@ function computeTrends(json) {
   // Calculate trend for past 24h
   const yesterday = records[0].OnPeakDownload || 0;
   const thedaybefore = records[1].OnPeakDownload || 0;
-  const percentagePreviousDay = numeral(1 - yesterday / thedaybefore).format('0%');
+
+  const percentagePreviousDay = thedaybefore > yesterday ? numeral(thedaybefore / yesterday).format('0%') : numeral(yesterday / thedaybefore).format('0%');
   const trendPreviousDay = thedaybefore > yesterday ? 'down' : 'up';
 
   // Calculate the trend for the past 7 days
   const past7days = records.slice(0, 7).map(record => (record.OnPeakDownload));
   const next7days = records.slice(7, 14).map(record => (record.OnPeakDownload));
-  const past7daysAvg = numeral(past7days.reduce((a, b) => (a + b)) / past7days.length).format('0.0');
-  const next7daysAvg = numeral(next7days.reduce((a, b) => (a + b)) / next7days.length).format('0.0');
-  const percentage7days = numeral(1 - next7daysAvg / past7daysAvg).format('0%');
+  const past7daysAvg = numeral(past7days.reduce((a, b) => (a + b)) / past7days.length).format('0.00');
+  const next7daysAvg = numeral(next7days.reduce((a, b) => (a + b)) / next7days.length).format('0.00');
+  const percentage7days = next7daysAvg > past7daysAvg ? numeral(next7daysAvg / past7daysAvg).format('0%') : numeral(past7daysAvg / next7daysAvg).format('0%');
   const trend7days = next7daysAvg > past7daysAvg ? 'down' : 'up';
 
   const effectiveRange = `${moment(records[records.length - 1].Date).format('MMMM Do')} - ${moment(records[0].Date).format('MMMM Do')}`
